f31406915bb2b327f0df2750fc04e8cb1e4bcf2d
ips3471 <ips3471@gmail.com>
2021-08-08 23:56:35 +0900

    add PageItemComponent

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,30 @@
+import { Component } from './components/component.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 // parent를 생성, page를 attach
 class App {
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
-        this.page = new PageComponent();
+        this.page = new PageComponent(); // <HTMLUlElement>
         this.page.attachTo(appRoot);
         
-        const image  = new ImageComponent('Image Title', 'https://picsum.photos/400/250');
-        image.attachTo(appRoot, 'beforeend');
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/400/250');
+        this.page.addChild(image);
 
         const video = new VideoComponent('Video Title', 'https://www.youtube.com/embed/w0cyk_1F-Rc');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
 
         const note = new NoteComponent('this is note Title', 'this is note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
 
         const toDo = new TodoComponent('Todo Title', 'Todo item');
-        toDo.attachTo(appRoot, 'beforeend');
+        this.page.addChild(toDo);
     }
 }
 
 new App(document.querySelector('.document')! as HTMLElement);
 // document section은 동적으로 생성되지 않는, 정적인 요소이므로 type assertion 사용
+
